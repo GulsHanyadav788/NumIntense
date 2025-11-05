c37e2b38e8012b3a71a43fb167cd3213c9bebe99
GulsHanyadav788 <gky735165@gmail.com>
2025-11-05 10:40:30 +0530

    Update abstractapi.py

--- apis/abstractapi.py
+++ apis/abstractapi.py
@@ -4,7 +4,7 @@ from colorama import Fore
 
 class AbstractAPI:
     def __init__(self, api_key):
-        self.api_key = Key:efd9c38411b1492ab17c244c0305c286
+        self.api_key = efd9c38411b1492ab17c244c0305c286
         self.base_url = "https://phonevalidation.abstractapi.com/v1/"
     
     def validate_number(self, number):
